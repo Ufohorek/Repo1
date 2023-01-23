da25eab80e4cb627d7ed3e3b06cebe11646b5cf2
Хмиловский Владислав Михайлович ПИБ12 <Ufo.horek@gmail.com>
2023-01-23 11:26:27 +0700

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -11,15 +11,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int year;
-            Console.Write("Введите код :");
+            Console.Write("Enter the Year :");
             year = Convert.ToInt32(Console.ReadLine());
             if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-                Console.WriteLine("{0} високосный", year);
+                Console.WriteLine("{0} is Leap Year", year);
             else
-                Console.WriteLine("{0} не високосный", year);
+                Console.WriteLine("{0} is not a Leap Year", year);
 
 
             Console.ReadLine();
         }
     }
-        }
+}
