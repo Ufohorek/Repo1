94e85f29b54fd81c9d18f28e4c828ec14b807da2
Хмиловский Владислав Михайлович ПИБ12 <Ufo.horek@gmail.com>
2023-01-23 10:25:52 +0700

    Add files via upload
    Проверка только на 4

---
+++ Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Enter the Year :");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0))
+                Console.WriteLine("{0} Високосный", year);
+            else
+                Console.WriteLine("{0} не високосный   ", year);
+
+
+            Console.ReadLine();
+        }
+    }
+        }
+        
+    
+
